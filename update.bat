abf95f21162b94981bb337d4f9917445ffc4be29
TRDC <vudi600@gmail.com>
2024-05-22 01:12:14 +0700

    Update bat

--- update.bat
+++ update.bat
@@ -1,1 +1,9 @@
-git pull https://github.com/Tarodictrl/better-genshin-impact-map-appsample.git
+@echo off
+
+if not exist .git (
+    git init
+    git remote add origin https://github.com/Tarodictrl/better-genshin-impact-map-appsample
+)
+
+git pull origin main
+pause
